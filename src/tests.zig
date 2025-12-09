d93fdef457ab8935110ba9730f2cb1c01e1acb65
niv <niv.schuman@gmail.com>
2025-12-09 15:32:35 +0200

    name entry test

--- src/tests.zig
+++ src/tests.zig
@@ -3,6 +3,6 @@ pub const cpu_test = @import("tests/cpu/cpu_test.zig");
 pub const instructions_test = @import("tests/cpu/instructions_test.zig");
 pub const memory_test = @import("tests/memory/memory_test.zig");
 
-test {
+test "tests entry" {
     @import("std").testing.refAllDecls(@This());
 }
